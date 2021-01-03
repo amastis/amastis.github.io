390f646b5da71752c5ec2b0b91e02158d8d48bce
amastis <37512516+amastis@users.noreply.github.com>
2021-01-03 14:03:00 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,12 +1,1 @@
-var xmlhttp = new XMLHttpRequest();
-var urlAdvice = "https://api.adviceslip.com/advice";
-xmlhttp.open("GET", urlAdvice, true);
-xmlhttp.send();
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        console.log(myArr);
-        document.getElementsByClassName("test")[0].innerText = myArr.slip.advice;
-    }
-};
