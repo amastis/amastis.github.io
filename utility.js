c296c6e5e8f5ba71a69e3843bc5e7178e2c7ae37
amastis <37512516+amastis@users.noreply.github.com>
2021-02-18 00:14:55 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,7 +1,15 @@
 const url = "https://www.reddit.com/r/LifeProTips/new.json";
 
+let headers = new Headers({
+    "Accept"       : "application/json",
+    "Content-Type" : "application/json",
+    "User-Agent"   : "MY-UA-STRING"
+});
+
 //testing how to fetch json data
-fetch(url)
-	.then(response => response.json())
+fetch(url, {
+    method  : 'GET', 
+    headers : headers 
+}).then(response => response.json())
 	.then(data => console.log(data))//.data.children[0].data.title + "\n" + data.data.children[0].data.url));
-  .catch(error => console.error(error));
+	.catch(error => console.error(error));
