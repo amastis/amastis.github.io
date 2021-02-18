3c7f3f8dbdf7f5c2633bf13f6a665f0356c8271c
amastis <37512516+amastis@users.noreply.github.com>
2021-02-18 00:12:54 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,1 +1,7 @@
+const url = "https://www.reddit.com/r/LifeProTips/new.json";
 
+//testing how to fetch json data
+fetch(url)
+	.then(response => response.json())
+	.then(data => console.log(data))//.data.children[0].data.title + "\n" + data.data.children[0].data.url));
+  .catch(error => console.error(error));
